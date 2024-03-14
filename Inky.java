a9707e00370fc2d50e7b819d45313e3b2213e8c0
AbdurRahman <abdhurrahman200011@gmail.com>
2024-03-14 19:56:49 +0530

    code pretify

--- Inky.java
+++ Inky.java
@@ -1,24 +1,24 @@
-public class Inky{
+public class Inky {
 
-public Inky(String Z){
-	System.out.println(Z);
-}
+	public Inky(String Z) {
+		System.out.println(Z);
+	}
 
-public Inky(String X, String Y, String V){
-	System.out.println(Y + " " + X);
-	System.out.println(X + " " + V);
-	System.out.println(Y + " " + X + " " + V);
-}
+	public Inky(String X, String Y, String V) {
+		System.out.println(Y + " " + X);
+		System.out.println(X + " " + V);
+		System.out.println(Y + " " + X + " " + V);
+	}
 
-public static void main(String args[]){
-	String pinky = "donky";
+	public static void main(String args[]) {
+		String pinky = "donky";
 
-	Inky ponky = new Inky(pinky);
+		Inky ponky = new Inky(pinky);
 
-	String blinky = "pinky";
-	String rinky = "monky";
+		String blinky = "pinky";
+		String rinky = "monky";
 
-	Inky ponky02 = new Inky(pinky, blinky, rinky);
+		Inky ponky02 = new Inky(pinky, blinky, rinky);
 
-}
+	}
 }
